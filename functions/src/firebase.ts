eaa2e2003be88fd996d47478c0ebf15364d0da82
admlucassabel <admlucassabel@gmail.com>
2022-07-15 10:43:44 -0300

    firebase

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -2,23 +2,21 @@ import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    privateKey: functions.config().private.key.replace(/\\n/g, '\n'),
-    projectId: functions.config().project.id,
-    clientEmail: functions.config().client.email
-  }),
-  databaseURL: 'https://[prova-carrinho].firebaseio.com'
+    credential: admin.credential.cert({
+        privateKey: functions.config().private.key.replace(/\\n/g, '\n'),
+        projectId: functions.config().project.id,
+        clientEmail: functions.config().client.email
+    }),
+    databaseURL: 'https://prova-carrinho.firebaseio.com'
 })
 
 const db = admin.firestore()
 const entry = db.collection('entries').doc()
-
 const entryObject = {
-  id: entry.id,
-  title: 'entry title here',
-  text: 'entry text here'
+    id: entry.id,
+    title: 'entry title here',
+    text: 'entry text here'
 }
 
-entry.set(entryObject)
-
+entry.create({ entryObject })
 export { admin, db }
