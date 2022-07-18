7e59402ae0b75efcd8935aafbbe4b80fc760adb9
admlucassabel <admlucassabel@gmail.com>
2022-07-17 21:14:08 -0300

    Versão Final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import '../styles/Footer.css';
 import '../styles/Font.css';
 import { Col } from 'react-bootstrap';
+import { useLocation } from "react-router-dom";
 
 function Footer() {
+  const { state } = useLocation();
+  var frete = <></>
+  if(state.value > 1000){
+    frete = <h6>Parabéns, sua compra tem frete grátis!</h6>;
+  }
+
   return (
     <div className='container-footer'>
       <div className="row">
         <Col className='container-total' xs={8}><h3>Total</h3></Col>
-        <Col className='container-price' xs={4}><h3>R$ 00</h3></Col>
+        <Col className='container-price' xs={4}><h3>R$ {state.value/100}</h3></Col>
+        <Col className='container-frete' xs={12}>{frete}</Col>
       </div>
     </div>
   );
--- src/components/Products_list.jsx
+++ src/components/Products_list.jsx
@@ -1,12 +1,9 @@
 
 import React from 'react';
-import '../styles/Header.css';
+import '../styles/Product_list.css';
 import '../styles/Font.css';
 import { Col } from 'react-bootstrap';
 import { useLocation } from "react-router-dom";
-
-// console.log(this.props)
-console.log("TESTE")
 
 function Products() {
   const { state } = useLocation();
@@ -14,10 +11,19 @@ function Products() {
 
   return (
     <div className="container-list">{state.items.map((item, index) => {
-        return (
-            <img key={item.id} src={item.imageUrl} alt="image" />
-        )
-      })}
+      return (
+        <div className='row product'>
+          <Col md={2} xs={6} sm={4}><img key={item.length} src={item.imageUrl} alt="image" /></Col>
+          <Col className='product-name' md={10} sm={8} xs={6}>
+            <div className='row desc'>
+              <Col className='product-name' xs={12}><h4>{item.name}</h4></Col>
+              <Col className='product-name h6' xs={12}><h6>R$ {item.listPrice/100}</h6></Col>
+              <Col className='product-name' xs={12}><h5>R$ {(item.listPrice+(item.priceTags[0].value))/100}</h5></Col>
+            </div>
+          </Col>
+        </div>
+      )
+    })}
     </div>
   );
 }
